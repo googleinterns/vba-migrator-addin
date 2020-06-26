2f502e94297dafde9b58341fbd764d60caaf34d5
jyokush <jyokush@google.com>
2020-06-26 14:43:42 +0530

    Update hittingEndPoint.vb

--- hittingEndPoint.vb
+++ hittingEndPoint.vb
@@ -3,10 +3,9 @@ Imports System.Net
 Imports System.IO
 Imports System.Text
 Module hittingEndPoint
-    'For getAuthorizationToken() function see this:
+    
+    'TODO: To understand "getAuthorizationToken()" function see this:
     'https://www.example-code.com/vbnet/hmrc_oauth2_access_token.asp 
-    'only need to change "scope" for which auth token needed
-    'and "clientId" and "clientSecretId".
     Private Function getAuthorizationToken() As String
         Dim glob As New Chilkat.Global
         Dim success As Boolean = glob.UnlockBundle("Anything for 30-day trial")
@@ -18,7 +17,7 @@ Module hittingEndPoint
         oauth2.ListenPort = 55568
         oauth2.AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/auth"
         oauth2.TokenEndpoint = "https://oauth2.googleapis.com/token"
-        ' Replace these with actual values.
+        'TODO: set your "clientId" and clientSecretId".
         oauth2.ClientId = "601010958158-ri1h9bipsbkfjip0qjhcnatfhdupnn08.apps.googleusercontent.com"
         oauth2.ClientSecret = "eeDXXNKGky5C7kX0SFwmsGXZ"
         oauth2.CodeChallenge = True
@@ -60,9 +59,8 @@ Module hittingEndPoint
 
     Public Function callSheetsAPI(ByRef fileId As String) As List(Of String)
         Dim lines As List(Of String) = Nothing
-        'Get Authorize Token
+        'Calls for Authorization Token.
         Dim _bearerToken As String = getAuthorizationToken()
-        'If "_bearerToken" then get out of the function no need to request.
         If _bearerToken <> "" Then
             Dim myUri As New Uri("https://docs.google.com/spreadsheets/vbaprocessfile?fid=" + fileId)
             Dim myWebRequest = System.Net.HttpWebRequest.Create(myUri)
@@ -71,81 +69,78 @@ Module hittingEndPoint
             myHttpWebRequest.PreAuthenticate = True
             myHttpWebRequest.Headers.Add("Authorization", "Bearer " & _bearerToken)
             Dim myWebResponse As HttpWebResponse = myWebRequest.GetResponse()
-            'Collect response after request.
+            'Collectting the response after request.
             Dim responseStream As Stream = myWebResponse.GetResponseStream()
             If responseStream Is Nothing Then
                 MessageBox.Show("Api didn't respond.")
                 Return lines
             End If
-            'Create file to store data from response stream.
+            'Creating a file to store data from the response stream.
             Dim Json As New FileStream("D:\Json.txt", FileMode.Create)
-            'Declare of local variable.
             Dim read As Byte() = New Byte(255) {}
             Dim count As Integer = responseStream.Read(read, 0, read.Length)
-            'Write in file.
+            'Writing the response data in the file created above.
             While count > 0
                 Json.Write(read, 0, count)
                 count = responseStream.Read(read, 0, read.Length)
             End While
-            'Close everything
+            'Closing the variables.
             Json.Close()
             responseStream.Close()
             myWebResponse.Close()
-            'Call the function parseTheFile() to get the information needed from the downloaded file.
+            'Calling the function "parseTheFile()" to get the information needed from the downloaded file.
             lines = parseTheFile()
         End If
         Return lines
     End Function
 
     Private Function parseTheFile() As List(Of String)
-        'Declare location variable need to store and read the file.
+        'Declaration of local variable needed to store and read the file.
         Dim lines As New List(Of String)
         Dim FileData As String = String.Empty
         Dim SReader As IO.StreamReader = Nothing
         Try
-            'Read the file which is download after hitting the Endpoint.
+            'Reading the file which was download after hitting the Endpoint.
             SReader = New IO.StreamReader("D:\xxx.txt")
-            'Read all data of the file and put it into a string.
+            'Putting the file data into a string.
             Do Until SReader.EndOfStream
                 FileData &= SReader.ReadLine()
             Loop
-            'Catch exception while reading the file
+            'Catching exception while reading the file
         Catch ex As Exception
             MessageBox.Show("Error: " & ex.Message)
             Return lines
         Finally
-            'Close Stream Reader.
+            'Closing Stream Reader.
             If SReader IsNot Nothing Then
                 SReader.Close()
                 SReader.Dispose()
             End If
-        End Try
-        'Define local variable need to parse the file. 
+        End Try 
         Dim index As Integer
         Dim tempStr As String = Nothing
         For index = 0 To FileData.Length - 1 Step +1
             If FileData.Chars(index) = """" Then
                 index += 1
-                'Take all string present in double quote in file for example: "API_LINE/chart.visible/blad4 3".
+            'Taking all the string present in double quote in the file for example: "API_LINE/chart.visible/blad4 3".
                 Do While FileData.Chars(index) <> """"
                     tempStr += FileData.Chars(index)
                     index += 1
                 Loop
-                'Define local variable array of string.
                 Dim arrayStr() As String
-                'Split the above string on the basis of '/'.
+            'Splitting the above string on the basis of '/'.
                 arrayStr = Split(tempStr, "/")
-                'Check the condition to get the only the information needed. 
+            'Checking the condition to get only the information needed. 
                 If arrayStr.Length >= 2 Then
                     If arrayStr(0) = "API_LINE" Then
-                        'Last word in API_LINE contain module name and line number so need to split it.
+                        'Last word in API_LINE contains module name and line number so need to split it on the basis of " ".
                         Dim myModuleInfo() As String = Split(arrayStr(2), " ")
                         Dim putValue As String
                         putValue = arrayStr(1) + "," + myModuleInfo(0) + "," + myModuleInfo(1)
-                        'Add the information in list.
+                        'Adding the information in list.
                         lines.Add(putValue)
                     ElseIf arrayStr(0) = "SUPPORTED" Then
-                        'Iterate through the list to delete the api which is supported.
+                    'Iterating through the list to delete the API which is supported.
                         For i As Integer = lines.Count - 1 To 0 Step -1
                             Dim getWord() As String
                             getWord = Split(lines.Item(i), ",")
@@ -159,7 +154,7 @@ Module hittingEndPoint
                         arrayStr(0) = "NOT_SUPPORTED" Or arrayStr(0) = "UNKNOWN" Or
                         arrayStr(0) = "SUPPORTED_AND_IMPLEMENTED" Or arrayStr(0) = "COM_API" Or
                         arrayStr(0) = "INVALIDATED" Then
-                        'Iterate through the list and find that api whose support is this and append it into it.
+                        'Iterating through the list and locating the APIs whose support type is this and appending them into corresponding index.
                         For i As Integer = 0 To lines.Count - 1 Step +1
                             Dim getWord() As String
                             getWord = Split(lines.Item(i), ",")
@@ -170,11 +165,11 @@ Module hittingEndPoint
                         Next i
                     End If
                 End If
-                'Reinitialize the temporary string to make it empty for next turn.
+                'Reinitialization of the temporary string to empty it for next turn.
                 tempStr = Nothing
             End If
         Next index
-        'Return data to show in grid, if it is empty then one message box will be seen with proper message.
+        'Returning the data.
         Return lines
     End Function
 
