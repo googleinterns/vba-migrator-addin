ecc3dc53e48e3aa8f488a4ad2547f74640d8e0cf
jyokush <jyokush@google.com>
2020-06-29 06:49:12 +0530

    click event of button and window initialization

--- Connect.vb
+++ Connect.vb
@@ -19,7 +19,12 @@ Public Class Connect
     Private _AddIn As AddIn
     ' Buttons created by the add-in
     Private WithEvents _Button As CommandBarButton
-
+     'Window created by add-in
+    Private _ApiSummaryWindow As Window
+    Dim count As Integer = 1
+    Dim lines As List(Of String) = Nothing
+    Dim fileId As String = ""
+    
     ''' <summary>
     ''' Implements the OnConnection method of the IDTExtensibility2 interface.
     ''' Receives notification that the Add-in is being loaded.
@@ -58,7 +63,7 @@ Public Class Connect
     ''' Describes how the Add-in is being unloaded.
     ''' </param>
     ''' <param name="custom">
-    ''' Array of parameters that are host application specific.
+    ''' Array of parameters that are host application-specific.
     ''' </param>
     ''' <seealso class='IDTExtensibility2' />
     Private Sub OnDisconnection(RemoveMode As Extensibility.ext_DisconnectMode,
@@ -80,7 +85,7 @@ Public Class Connect
     ''' Receives notification that the host application has completed loading.
     ''' </summary>
     ''' <param name="custom">
-    ''' Array of parameters that are host application specific.
+    ''' Array of parameters that are host application-specific.
     ''' </param>
     ''' <seealso class='IDTExtensibility2' />
     Private Sub OnStartupComplete(ByRef custom As System.Array) _
@@ -143,6 +148,82 @@ Public Class Connect
         Catch ex As Exception
             MessageBox.Show(ex.ToString())
         End Try
+    End Sub
+    
+     ''' <summary>
+    ''' Host a window by UserControlHost.
+    ''' </summary>
+    ''' <param name="toolWindowCaption">
+    ''' String you need to put as a header on the window.
+    ''' </param>
+    ''' <param name="toolWindowGuid">
+    ''' This uniquely identified a particular window can be used to store 
+    ''' information of it like size, position, etc.
+    ''' </param>
+    ''' <param name="toolWindowUserControl">
+    ''' Windows to host.
+    ''' </param>
+    Private Function CreateToolWindow(ByVal toolWindowCaption As String, ByVal toolWindowGuid As String,
+      ByVal toolWindowUserControl As UserControl) As Window
+        Dim userControlObject As Object = Nothing
+        Dim userControlHost As UserControlHost
+        Dim _SummaryWindow As Window
+        Dim progId As String
+        ' IMPORTANT: ensure that you use the same ProgId value used in the ProgId attribute of the UserControlHost 
+        progId = "MyVBAAddin.UserControlHost"
+        _SummaryWindow = _VBE.Windows.CreateToolWindow(_AddIn, progId, toolWindowCaption, toolWindowGuid, userControlObject)
+        userControlHost = DirectCast(userControlObject, UserControlHost)
+        _SummaryWindow.Visible = True
+        userControlHost.AddUserControl(toolWindowUserControl)
+        Return _SummaryWindow
+    End Function
+
+    ''' <summary>
+    ''' Implementation of the click event of the button.
+    ''' </summary>
+    ''' <param name="Ctrl"></param>
+    ''' <param name="CancelDefault"></param>
+    Private Sub _Button_Click(Ctrl As Microsoft.Office.Core.CommandBarButton,
+      ByRef CancelDefault As Boolean) Handles _Button.Click
+        'If the button is clicked first time.
+        If count = 1 Then
+            'Make a copy of the file active in excel.
+            My.Computer.FileSystem.CopyFile(_VBE.ActiveVBProject.FileName, "D:\GoogleDrive.xlsm", True)
+            Dim letsTry As uploadFileToDrive = New uploadFileToDrive()
+            'Upload the file on drive.
+            fileId = letsTry.UploadFile("D:\GoogleDrive.xlsm")
+            'Delete that file.
+            My.Computer.FileSystem.DeleteFile("D:\GoogleDrive.xlsm")
+            'If no error ocurred then fileId is not empty.
+            If fileId <> "" Then
+                lines = hittingEndPoint.callSheetsAPI(fileId)
+            Else
+                Exit Sub
+            End If
+        End If
+        'if the data list count item is not zero then all Api in the file is not supported.
+        If lines.Count <> 0 Then
+            Dim userControlObject As Object = Nothing
+            Dim userControlToolWindow1 As UserControlToolWindow1
+            Try
+                    'If button is clicked the first time then window needs to initialize.
+                If _ApiSummaryWindow Is Nothing Then
+                    userControlToolWindow1 = New UserControlToolWindow1()
+                    _ApiSummaryWindow = CreateToolWindow("Report of API used in this project", "0EB93108-D229-4F6F-82C5-0B96AFFBB9C5", userControlToolWindow1)
+                    userControlToolWindow1.Initialize(_VBE, lines)
+                    count += 1
+                Else
+                    'If the window is previously initialized then make it visible.
+                    _ApiSummaryWindow.Visible = True
+                End If
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString)
+            End Try
+            'if the data list is empty and filed id is not empty then all API used in the file is supported.
+        ElseIf fileId <> "" Then
+            MessageBox.Show("Fully Compatible!!")
+            count += 1
+        End If
     End Sub
 
 End Class
