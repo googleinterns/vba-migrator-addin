338420596cd56599ca7018556a385a9268310dd8
jyokush <jyokush@google.com>
2020-07-09 11:58:59 +0530

    Update uploadFileToDrive.vb

--- uploadFileToDrive.vb
+++ uploadFileToDrive.vb
@@ -1,3 +1,17 @@
+'Copyright 2020 Google LLC
+'
+'Licensed under the Apache License, Version 2.0 (the "License");
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the License at
+'
+'   https://www.apache.org/licenses/LICENSE-2.0
+'
+'Unless required by applicable law or agreed to in writing, software
+'distributed under the License is distributed on an "AS IS" BASIS,
+'WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'See the License for the specific language governing permissions and
+'limitations under the License.
+
 Imports Google.Apis.Drive.v2
 Imports Google.Apis.Auth.OAuth2
 Imports Google.Apis.Services
@@ -8,7 +22,7 @@ Imports Google.Apis.Drive.v2.Data
 '''To upload the file to Drive, "UploadFile("file path","userClientId","userClientSecretId")" member function of this class
 ''' have to call by passing the path of the file to upload and client credentials. Follow the instructions in README.md to get the credentials. 
 ''' </summary>
-Public Class uploadFileToDrive
+Public Module uploadFileToDrive
     Private Service As DriveService = New DriveService()
     'Create Drive API service.
     Private Sub CreateService(ByRef userClientId As String , ByRef userClientSecretId As String)
@@ -52,4 +66,4 @@ Public Class uploadFileToDrive
             Return file.Id
         End If
     End Function
-End Class
+    End Module
